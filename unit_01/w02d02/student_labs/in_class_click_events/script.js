65e3b22bbcb6007dcf696f71f0ae8f308874c39c
ddmilo <ddmilo1029@gmail.com>
2017-03-02 10:35:41 -0500

    *

--- unit_01/w02d02/student_labs/in_class_click_events/script.js
+++ unit_01/w02d02/student_labs/in_class_click_events/script.js
@@ -1,3 +1,7 @@
 window.onload = function() {
-  
+  var buttonOne = document.querySelector('buttonOne');
+
+  buttonOne.addEventListener('click', function(event){
+  	consol.log("You have clicked button one");
+  });
 }
