e506a155da2555f5f8662a77b1182fae73bc958b
ddmilo <ddmilo1029@gmail.com>
2017-03-14 17:54:31 -0400

    added get routes

--- unit_02/w06d02/homework/pizza-express/app.js
+++ unit_02/w06d02/homework/pizza-express/app.js
@@ -3,5 +3,19 @@ var app = express();
 var port = 3000;
 
 app.listen(port, function(){
-	console.log('I hear you');
+	console.log('I hear you');	
 });
+
+app.get('/', function(req, res){
+	res.send("Welcome to Pizza Express!");
+});
+
+app.get('/topping/:type', function(req, res, next){
+	res.send(req.params.type + '!' + ' ' + 'Good Choice!');
+
+});
+
+app.get('/order/:amount/:size', function (req, res, next) {
+	res.send("You ordered" + ' ' + req.params.amount + ' ' + req.params.size + ' ' + "Pizzas");
+});
+
