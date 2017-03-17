040ab85c47c3f7bc9b436f24cb6951486d0cd963
ddmilo <ddmilo1029@gmail.com>
2017-03-17 09:14:41 -0400

    .

--- unit_02/w06d04/morning_exercise/clocks.js
+++ unit_02/w06d04/morning_exercise/clocks.js
@@ -1,8 +1,26 @@
 var calculate = function(hourhand, minutehand) {
+	// var minutesPerHour = 60;
+	// var hoursOnClock = 12;
+	var minuteAngle = (hourhand * 60 + minutehand) * 6;
+	var hourAngle = (hourhand * 60 + minutehand) * 0.5;
+	return hourAngle - 360
 
 };
+
+
+
+//Calculate the two hands of a clock into degrees
+//A full rotation is 360 degrees
+//There are 12 hours on a clock
+//Hour hand drifts
+//60 minutes each hour
+//60 seconds to a minute
+//minute hand moves 360 degree in 60 minutes(6 degree in one minute)
+//hour hand moves 360 in 12 hours(.5 degree in one minute)
+
 
 // DO NOT EDIT BELOW THIS LINE //
 module.exports = {
   calculate: calculate
 };
+w
