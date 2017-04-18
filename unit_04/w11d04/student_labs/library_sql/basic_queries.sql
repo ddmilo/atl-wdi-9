1da0bf082438afb2972fbc5de9d6cba6dcc65868
ddmilo <ddmilo1029@gmail.com>
2017-04-17 23:31:54 -0400

    idk

--- unit_04/w11d04/student_labs/library_sql/basic_queries.sql
+++ unit_04/w11d04/student_labs/library_sql/basic_queries.sql
@@ -2,30 +2,31 @@
 -- Unless otherwise stated, all queries should return all columns
 
 -- Get all information about all authors
-
+SELECT * FROM authors;
 -- Get just the name and birth year of all authors
-
+SELECT name, birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
-
+SELECT * FROM authors WHERE birth_year > 1899;
 -- Get all authors born in the USA
-
+SELECT * FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
-
+SELECT * FROM books WHERE publication_date = 1985;
 -- Get all books published before 1989
-
+SELECT * FROM books WHERE publication_date < 1989;
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT * FROM books LIKE title = 'the';
 -- Add yourself as an author
-
+INSERT INTO authors(name, nationality, birth_year) VALUES ('Dan Milo', 'United States of America', 1990);
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
-
+INSERT INTO books(tite, publication_date, author_id) VALUES ('SQL stuff', 2017, 10);
 -- Update one of your books to have a new title
-
+UPDATE books SET title = 'new name' WHERE title = 'SQL stuff';
 -- Delete your books
-
+DELETE FROM books WHERE author_id = 10;
 -- Delete your author entry
+DELETE FROM authors WHERE name = 'Dan Milo';
